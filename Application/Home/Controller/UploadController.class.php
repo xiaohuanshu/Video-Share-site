a66f295dae14d9f7a5cc8336bc967024ff188924
xiaohuanshu <xiaohuanshu@gmail.com>
2015-06-10 16:19:17 +0800

    change uploader

--- Application/Home/Controller/UploadController.class.php
+++ Application/Home/Controller/UploadController.class.php
@@ -5,67 +5,7 @@ class UploadController extends GlobalAction
 {
     public function _initialize()
     {
-        parent::_initialize();
-    }
-    public function uploadview($movieid)
-    {
-        list($uid, $username) = getuserinfo();
-        if (empty($uid)) {
-            $this->error('您还没有登录');
-        }
-        $this->assign('movieid', I('get.movieid'));
-        $this->display();
-    }
-    public function deal($movieid)
-    {
-        list($uid, $username) = getuserinfo();
-        if (empty($uid)) {
-            $this->error('您还没有登录');
-        }
-        for ($i = 0; $i <= intval($_POST['uploader_count']) - 1; $i++) {
-            //$uploader_tmpname[$i] = $_POST['uploader_' . $i . '_tmpname'];
-            //$uploader_name[$i]    = $_POST['uploader_' . $i . '_name'];
-            $uploader_tmpname[$i] = I('post.uploader_' . $i . '_tmpname');
-            $uploader_name[$i]    = I('post.uploader_' . $i . '_name');
-        }
-        $this->assign('uploader_name', $uploader_name);
-        $this->assign('uploader_tmpname', $uploader_tmpname);
-        $this->assign('uploader_count', I('post.uploader_count'));
-        $this->assign('movieid', I('get.movieid'));
-        $this->display();
-    }
-    public function deal2($movieid)
-    {
-        list($uid, $username) = getuserinfo();
-        if (empty($uid)) {
-            $this->error('您还没有登录');
-        }
-        $locallist = M('localvideo');
-        for ($i = 0; $i <= intval($_POST['uploader_count']) - 1; $i++) {
-            $c['userid']  = $uid;
-            $c['movieid'] = $movieid;
-            $c['url']     = I('post.uploader_' . $i . '_tmpname');
-            $c['title']   = I('post.uploader_' . $i . '_name');
-			if (in_array($uid,C('ADMIN_ID'))) {
-	        	$c['verify']  = 1;
-				changeuploadstatus($movieid);
-			}else{
-				$c['verify']  = 0;
-			}
-            $c['time']    = date('Y-m-d H:i:s');
-            //判断能否在线播放
-            list($video, $audio) = videoinfo(I('post.uploader_' . $i . '_tmpname'));
-            if (strpos($video, "h264")) { //&&strpos($audio,"aac")){
-                $c['online'] = 1;
-				//视频截图
-				videoshot(I('post.uploader_' . $i . '_tmpname'));
-            } else {
-                $c['online'] = 0;
-            }
-            $locallist->add($c);
-        }
-        addtimeline($movieid, '上传视频', I('post.uploader_' . $i . '_tmpname'), $username, 'fa fa-cloud-upload time-icon bg-dark');
-        $this->success("上传成功", U("movie/show?id=" . $movieid));
+        //parent::_initialize();
     }
     public function poster($movieid)
     {
@@ -139,5 +79,185 @@ class UploadController extends GlobalAction
 		header("Content-type: image/jpg");
 		echo  $data;
 	}
+
+    public function uploader()
+    {
+// Make sure file is not cached (as it happens for example on iOS devices)
+        header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+        header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+        header("Cache-Control: no-store, no-cache, must-revalidate");
+        header("Cache-Control: post-check=0, pre-check=0", false);
+        header("Pragma: no-cache");
+
+        if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+            exit; // finish preflight CORS requests here
 }
-?>
+
+// 5 minutes execution time
+        @set_time_limit(5 * 60);
+
+// Settings
+// $targetDir = ini_get("upload_tmp_dir") . DIRECTORY_SEPARATOR . "plupload";
+        $targetDir = C('targetDir');
+        $uploadDir = C('uploadDir');
+
+        $cleanupTargetDir = true; // Remove old files
+        $maxFileAge = 5 * 3600; // Temp file age in seconds
+
+
+// Create target dir
+        if (!file_exists($targetDir)) {
+            @mkdir($targetDir);
+        }
+
+// Create target dir
+        if (!file_exists($uploadDir)) {
+            @mkdir($uploadDir);
+        }
+
+// Get a file name
+        if (isset($_REQUEST["newname"])) {
+            $fileName = $_REQUEST["newname"];
+        } else {
+            die('{"jsonrpc" : "2.0", "error" : {"code": 100, "message": "Name error."}, "id" : "id"}');
+        }
+        /*
+                $md5File = @file('md5list2.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+                $md5File = $md5File ? $md5File : array();
+
+                if (isset($_REQUEST["md5"]) && array_search($_REQUEST["md5"], $md5File) !== FALSE) {
+                    die('{"jsonrpc" : "2.0", "result" : null, "id" : "id", "exist": 1}');
+                }
+        */
+
+        //check file ext
+        $allow_file = explode(",", C('allow_ext')); //允许上传的文件类型组
+        $new_upload_file_ext = strtolower(end(explode(".", $fileName))); //取得被.隔开的最后字符串
+        if (!in_array($new_upload_file_ext, $allow_file)) { //如果不在组类，提示处理
+            die('{"jsonrpc" : "2.0", "error" : {"code": -601, "message": "File extension error."}, "id" : "id"}');
+        }
+
+        $filePath = $targetDir . DIRECTORY_SEPARATOR . $fileName;
+        $uploadPath = $uploadDir . DIRECTORY_SEPARATOR . $fileName;
+
+// Chunking might be enabled
+        $chunk = isset($_REQUEST["chunk"]) ? intval($_REQUEST["chunk"]) : 0;
+        $chunks = isset($_REQUEST["chunks"]) ? intval($_REQUEST["chunks"]) : 0;
+
+
+// Remove old temp files
+        if ($cleanupTargetDir) {
+            if (!is_dir($targetDir) || !$dir = opendir($targetDir)) {
+                die('{"jsonrpc" : "2.0", "error" : {"code": 100, "message": "Failed to open temp directory."}, "id" : "id"}');
+            }
+
+            while (($file = readdir($dir)) !== false) {
+                $tmpfilePath = $targetDir . DIRECTORY_SEPARATOR . $file;
+
+                // If temp file is current file proceed to the next
+                if ($tmpfilePath == "{$filePath}.part") {
+                    continue;
+                }
+
+                // Remove temp file if it is older than the max age and is not the current file
+                if (preg_match('/\.part$/', $file) && (filemtime($tmpfilePath) < time() - $maxFileAge)) {
+                    @unlink($tmpfilePath);
+                }
+            }
+            closedir($dir);
+        }
+
+
+// Open temp file
+        if (!$out = @fopen("{$filePath}.part", $chunks ? "ab" : "wb")) {
+            die('{"jsonrpc" : "2.0", "error" : {"code": 102, "message": "Failed to open output stream."}, "id" : "id"}');
+        }
+
+        if (!empty($_FILES)) {
+            if ($_FILES["file"]["error"] || !is_uploaded_file($_FILES["file"]["tmp_name"])) {
+                die('{"jsonrpc" : "2.0", "error" : {"code": 103, "message": "Failed to move uploaded file."}, "id" : "id"}');
+            }
+
+            // Read binary input stream and append it to temp file
+            if (!$in = @fopen($_FILES["file"]["tmp_name"], "rb")) {
+                die('{"jsonrpc" : "2.0", "error" : {"code": 101, "message": "Failed to open input stream."}, "id" : "id"}');
+            }
+        } else {
+            if (!$in = @fopen("php://input", "rb")) {
+                die('{"jsonrpc" : "2.0", "error" : {"code": 101, "message": "Failed to open input stream."}, "id" : "id"}');
+            }
+        }
+
+        while ($buff = fread($in, 4096)) {
+            fwrite($out, $buff);
+        }
+
+        @fclose($out);
+        @fclose($in);
+
+// Check if file has been uploaded
+        if (!$chunks || $chunk == $chunks - 1) {
+            /*
+            array_push($md5File, mymd5($uploadPath));
+            $md5File = array_unique($md5File);
+            file_put_contents('md5list2.txt', join($md5File, "\n"));*/
+            //var_dump($_GET);
+            $movieid = I('request.movieid');
+            list($uid, $username) = getuserinfo();
+            if (empty($uid)) {
+                die('{"jsonrpc" : "2.0", "error" : {"code": 100, "message": "Not login."}, "id" : "id"}');
+            }
+            $locallist = M('localvideo');
+            $c['userid'] = $uid;
+            $c['movieid'] = $movieid;
+            $c['url'] = $fileName;
+            if (!empty($_REQUEST['editname'])) {
+                $c['title'] = I('request.editname');
+            } else {
+                die('{"jsonrpc" : "2.0", "error" : {"code": 100, "message": "No name."}, "id" : "id"}');
+            }
+            if (in_array($uid, C('ADMIN_ID'))) {
+                $c['verify'] = 1;
+                changeuploadstatus($movieid);
+            } else {
+                $c['verify'] = 0;
+            }
+            $c['time'] = date('Y-m-d H:i:s');
+            //判断能否在线播放
+            list($video, $audio) = videoinfo($fileName);
+            if (strpos($video, "h264")) { //&&strpos($audio,"aac")){
+                $c['online'] = 1;
+                //视频截图
+                videoshot($fileName);
+            } else {
+                $c['online'] = 0;
+            }
+            $locallist->add($c);
+            addtimeline($movieid, '上传视频', $c['title'], $username, 'fa fa-cloud-upload time-icon bg-dark');
+            //$this->success("上传成功", U("movie/show?id=" . $movieid));
+
+            // Strip the temp .part suffix off
+            rename("{$filePath}.part", $filePath);
+            rename($filePath, $uploadPath);
+        }
+        /*
+                function mymd5($file)
+                {
+                    $fragment = 65536;
+
+                    $rh = fopen($file, 'rb');
+                    $size = filesize($file);
+
+                    $part1 = fread($rh, $fragment);
+                    fseek($rh, $size - $fragment);
+                    $part2 = fread($rh, $fragment);
+                    fclose($rh);
+
+                    return md5($part1 . $part2);
+                }*/
+
+
+// Return Success JSON-RPC response
+        die('{"jsonrpc" : "2.0", "result" : null, "id" : "id"}');
+    }
+}
